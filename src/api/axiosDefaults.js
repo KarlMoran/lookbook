6ad65433864982ee7d55dfa8b59dc5f94fc41077
Karl Moran <karlmoran06@gmail.com>
2022-10-14 12:05:26 +0000

    Add interpectors for request and response / Added useMemo to CurrentUserContext.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-lookbook.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
