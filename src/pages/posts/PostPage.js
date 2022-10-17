1fc9a093e49a5c1943703f642093214058a87dcd
Karl Moran <karlmoran06@gmail.com>
2022-10-17 11:38:46 +0000

    Created the PostPage and added a useEffect method

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useParams } from "react-router";
 
 function PostPage() {
   // Add your logic here
+  const {id} = useParams();
+  const [post, setPost] = useState({results:[]});
 
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{data:post}] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({results:[post]});
+        console.log(post);
+      } catch(err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  },[id]);
 
   return (
     <Row className="h-100">
