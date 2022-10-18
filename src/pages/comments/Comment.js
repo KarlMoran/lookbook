970bf1b7e3fb70d5dabfe92e99f1c783476111aa
Karl Moran <karlmoran06@gmail.com>
2022-10-18 14:01:11 +0000

    Made the comment component

---
+++ src/pages/comments/Comment.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Media } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import styles from "../../styles/Comment.module.css";
+
+// rafce
+
+const Comment = (props) => {
+    const { profile_id, profile_image, owner, updated_at, content } = props;
+
+    return (
+    <div>
+        <hr />
+        <Media>
+            <Link to={`/profiles/${profile_id}`}>
+                <Avatar src={profile_image} />
+            </Link>
+            <Media.Body className="align-self-center ml-2">
+                <span className={styles.Owner}>{owner}</span>
+                <span className={styles.Date}>{updated_at}</span>
+                <p>{content}</p>
+            </Media.Body>
+        </Media>
+    </div>
+    );
+};
+
+export default Comment;
