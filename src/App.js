560cea3fd7e65328714d7b1489d573346817eebc
Karl Moran <karlmoran06@gmail.com>
2022-10-18 13:06:02 +0000

    Created the PosteditForm to edit posts

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
+import PostEditForm from "./pages/posts/PostEditForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -26,29 +27,23 @@ function App() {
               <PostsPage message="No results found. Adjust the search keyword." />
             )}
           />
-          <Route
-            exact
-            path="/explore"
-            render={() => (
-              <PostsPage
-                message="No results."
-                filter={`owner__followed__owner__profile=${profile_id}&`}
-              />
-            )}
+          <Route exact path="/explore" render={() => (<PostsPage
+            message="No results."
+            filter={`owner__followed__owner__profile=${profile_id}&`}
           />
-          <Route
-            exact
-            path="/liked"
-            render={() => (
-            <PostsPage 
-            message="No results found. Adjust the search keyword or like a post."
-            filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+          )}
+          />
+          <Route exact path="/liked" render={() => (
+            <PostsPage
+              message="No results found. Adjust the search keyword or like a post."
+              filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
             />
-            )}
+          )}
           />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
