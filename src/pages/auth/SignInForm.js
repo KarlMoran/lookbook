f9d52bcf01a8ef7e4e1d007a38f8881d2e1a8c4f
Karl Moran <karlmoran06@gmail.com>
2022-10-21 11:44:04 +0000

    Removed useRedirect cause i couldn't signIn or signUp

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,11 +15,11 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
-import { useRedirect } from "../../hooks/useRedirect";
+// import { useRedirect } from "../../hooks/useRedirect";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
-  useRedirect("loggedIn");
+  // useRedirect("loggedIn");
 
   const [signInData, setSignInData] = useState({
     username: "",
